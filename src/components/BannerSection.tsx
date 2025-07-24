c8e35a72dcfc58256827a88ddfc429a7d1b2226b
石田淳 <ishidaatsushi@ishidaatsushinoMacBook-Pro.local>
2025-07-24 11:57:27 +0900

    fix: Remove unused Col import in BannerSection.tsx

--- src/components/BannerSection.tsx
+++ src/components/BannerSection.tsx
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row } from 'react-bootstrap';
 
 interface BannerSectionProps {
   imageUrl: string;
